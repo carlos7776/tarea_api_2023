9ce4e997ac36ca57c75b89c254a480372d2e92d1
carlos7776 <cravila091@gmail.com>
2023-01-30 17:30:52 -0500

    tareadelunesapi_julian

---
+++ picsum.js
@@ -0,0 +1,26 @@
+(function(){
+   const url = 'https://picsum.photos/v2';
+   fetch(`${url}/list`)  // llamado a api externo 
+    .then(resp => {
+        if(resp.ok && resp.status==200){
+            return resp.json();
+        }
+    })
+
+   .then(data => {
+        for(let item of data){
+            document.getElementById("contenido").innerHTML += 
+
+        `<div class="card" style="width: 18rem;">
+        <img src="${item.download_url}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${item.author} </h5>
+          <h6 class="card-subtitle mb-2 text-muted">${item.id}</h6>
+          <a href="${item.url}" class"card-link">${item.url}
+        </div>
+      </div>
+            `;
+        }
+   })
+   .catch(resp => console.log("error llamado de api"))
+    })()
